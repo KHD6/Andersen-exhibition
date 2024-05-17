22b62a33f4b9dfa46dbf534a99d3088177bd361f
KHD6 <168617446+KHD6@users.noreply.github.com>
2024-05-17 17:57:18 +0900

    0517_업데이트

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 $(document).ready(function(){
 
-/*profile 초상화 움직임 시작*/
- 
-/*profile 초상화 움직임 끝*/
+  /*작품 슬라이드 시작*/
+  var swiper3 = new Swiper(".fairytale-slide", {
+    grabCursor: true,
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+  });
+    /*작품 슬라이드 끝*/
 
 });
